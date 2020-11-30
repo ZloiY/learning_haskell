310b5596798f03804199cfba4b3392a4ae73774b
Maksim Bakhanovich <norfolf1000@gmail.com>
2020-11-30 01:43:17 -0800

    feat(LogAnalysis): complete exercises

--- src/LogAnalysis.hs
+++ src/LogAnalysis.hs
@@ -1,28 +1,42 @@
 module LogAnalysis
   ( parseMessage
   , parse
-  , createMessageTree
+  , buildMessageTree
+  , insertMessage
+  , inOrder
+  , whatWentWrong
   ) where
 
 import Log
 
 parseMessage :: String -> LogMessage
-parseMessage = aggregateMessage (LogMessage Info 0 "") . words
-  where aggregateMessage _ ("E":a:xs)                      = aggregateMessage (LogMessage (Error (read a)) 0 "") xs
-        aggregateMessage _ ("W":xs)                        = aggregateMessage (LogMessage Warning 0 "") xs
-        aggregateMessage _ ("I":xs)                        = aggregateMessage (LogMessage Info 0 "") xs
-        aggregateMessage (LogMessage Info _ _) (x:xs)      = LogMessage Info (read x) (unwords xs)
-        aggregateMessage (LogMessage Warning _ _) (x:xs)   = LogMessage Warning (read x) (unwords xs)
-        aggregateMessage (LogMessage (Error e) _ _) (x:xs) = LogMessage (Error e) (read x) (unwords xs)
+parseMessage = aggregateMessage . words
+  where aggregateMessage ("E":e:t:msg) = LogMessage (Error (read e)) (read t) (unwords msg)
+        aggregateMessage ("W":t:msg)   = LogMessage Warning (read t) (unwords msg)
+        aggregateMessage ("I":t:msg)   = LogMessage Info (read t) (unwords msg)
+        aggregateMessage msg           = Unknown (unwords msg)
 
 parse :: String -> [LogMessage]
 parse log = map parseMessage (lines log)
 
-createMessageTree :: [LogMessage] -> MessageTree
-createMessageTree = foldl findPlace Leaf
-  where 
-    findPlace (Node left message1@(LogMessage _ b1 _) right) message@(LogMessage _ b _)
-      | b > b1    = Node left message1 (findPlace right message)
-      | otherwise = Node (findPlace left message) message1 right
-    findPlace Leaf message = Node Leaf message Leaf
+insertMessage :: LogMessage -> MessageTree -> MessageTree
+insertMessage message@(LogMessage _ t _) (Node left message1@(LogMessage _ t1 _) right)
+  | t > t1    = Node left message1 (insertMessage message right)
+  | otherwise = Node (insertMessage message left) message1 right
+insertMessage message Leaf            = Node Leaf message Leaf
+insertMessage (Unknown _) messageTree = messageTree
 
+buildMessageTree :: [LogMessage] -> MessageTree
+buildMessageTree = foldl (flip insertMessage) Leaf
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf                      = []
+inOrder (Node left message right) = inOrder left ++ [message] ++ inOrder right
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = foldl errors [] . inOrder . buildMessageTree
+  where
+    errors messages (LogMessage (Error e) _ message)
+      | e >= 50   = messages ++ [message]
+      | otherwise = messages
+    errors messages _ = messages
